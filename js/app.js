cafd58617705097073fd980912bf6fec9258ebb1
lomholdt <jonaslomholdt@gmail.com>
2017-05-15 20:19:39 +0200

    Make super simple computation even simpler

--- js/app.js
+++ js/app.js
@@ -1,46 +1,44 @@
 new Vue({
   el: '#app',
-
   data: {
     seconds: 0,
     minutes: 0,
     hours: 0,
     days: 0,
     diff: 0,
-    returnDate: '2016-02-18 18:00',
+    interval: 1000,
+    returnDate: '2017-05-15 23:00',
   },
-
   methods: {
-    setDiff: function(){
+    setDiff: function() {
       var now = moment();
       var then = moment(this.returnDate);
       this.diff = then.diff(now);
     },
-    setTimeFromMillis: function(){
+    calculateTimeFromMillis: function(){
       this.seconds = parseInt((this.diff / 1000) % 60);
-      this.minutes = parseInt((this.diff / (1000*60)) % 60);
-      this.hours = parseInt((this.diff / (1000*60*60)) % 24);
-      this.days = parseInt((this.diff / (1000*60*60*24)));
+      this.minutes = parseInt((this.diff / (1000 * 60)) % 60);
+      this.hours = parseInt((this.diff / (1000 * 60 * 60)) % 24);
+      this.days = parseInt((this.diff / (1000 * 60 * 60 * 24)));
 
     },
-    updateTime: function(){
-      this.setDiff();
-      this.setTimeFromMillis();
+    updateTime: function() {
+      this.diff = this.diff - this.interval;
+      this.calculateTimeFromMillis();
     }
   },
-
-  ready: function(){
+  ready: function() {
     moment.locale('dk');
-    this.updateTime();
-    setInterval(this.updateTime, 1000);
+    this.setDiff();
+    setInterval(this.updateTime, this.interval);
   }
 });
 
 $('document').ready(function() {
-                $('.content').fadeIn(4000);
-                $('#app').tubular({
-                  videoId: 'A9Tp5fl18Ho',
-                  mute: false,
-                  start: 491
-                });
-        });
+  $('.content').fadeIn(4000);
+  $('#app').tubular({
+    videoId: 'A9Tp5fl18Ho',
+    mute: false,
+    start: 491
+  });
+});
